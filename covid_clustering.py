070906c4098c262a1cbe3f62f7134b60839b6931
Jesús Fernández-Bermejo Ruiz <jesus.fruiz@uclm.es>
2020-05-29 16:32:42 +0200

    created groups representatives

--- covid_clustering.py
+++ covid_clustering.py
@@ -14,6 +14,7 @@ from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 from sklearn.mixture import GaussianMixture
 
+# 1. Auxiliar Methods
 def bic(K, cidx, X):
     k = 0
     P = len(X.iloc[1,:])
@@ -38,6 +39,16 @@ def bic(K, cidx, X):
         k+=1
     return -2*sum(xi)+2*K*P*math.log(N) 
 
+def plot_3d(x, y, z, labels=None, cmap=None):
+    threedee = plt.figure().gca(projection='3d')
+    threedee.scatter(x, y, z, c=labels, cmap=None)
+    threedee.set_xlabel('Confirmed')
+    threedee.set_ylabel('Deaths')
+    threedee.set_zlabel('Recovered')
+    plt.show()    
+    
+
+# 2. Data reading and preprocessing
 covid_df = pd.read_csv("covid_19_clean_complete.csv")
 covid_df = covid_df.loc[covid_df['Date'] == '5/25/20']
 covid_df = covid_df.groupby('Country/Region').sum()
@@ -52,13 +63,8 @@ covid_df['Recovered'] = covid_df['Recovered']/covid_df['Population (2020)']
 
 covid_df = covid_df.drop(['Lat', 'Long', 'Population (2020)'], axis=1)
 
-
-threedee = plt.figure().gca(projection='3d')
-threedee.scatter(covid_df['Confirmed'], covid_df['Deaths'], covid_df['Recovered'])
-threedee.set_xlabel('Confirmed')
-threedee.set_ylabel('Deaths')
-threedee.set_zlabel('Recovered')
-plt.show()
+# 3. Plotting Data
+plot_3d(covid_df['Confirmed'], covid_df['Deaths'], covid_df['Recovered'])
 
 plt.scatter(covid_df['Confirmed'], covid_df['Deaths'])
 plt.plot()
@@ -68,7 +74,7 @@ plt.xlabel('Confirmed')
 plt.ylabel('Deaths')
 plt.show()
 
-#2.1 Calculo número ideal de clusters
+# 4. Ideal cluster number
 K=2
 BIC = []
 
@@ -84,9 +90,9 @@ plt.scatter(X, BIC)
 plt.plot()
 plt.show()
 
-K = 3
+K = 3 # Three is the ideal number
 
-#2.2 Detección y Eliminación de Outliers: Jackknife
+# 5 Outliers Detection: Jackknife
 data = covid_df.drop(['Country/Region'], axis=1)
 kmeans = KMeans(n_clusters=K, init='random', n_init=40)
 SSE = dict()
@@ -107,18 +113,22 @@ for i in range(len(covid_df)):
 #estimator = PCA (n_components = 2)
 #X_pca = estimator.fit_transform(data)
 #print(estimator.explained_variance_ratio_) 
-X_pca = covid_df.drop(["Deaths"], axis = 1)
+X_pca = covid_df.drop(["Recovered"], axis = 1)
 
+out_label = []
 for i in range(len(X_pca)):
     if i in outliers:
         col = 'r'
+        out_label.append(1)
     else:
         col = 'w'
+        out_label .append(2)
         
-    plt.plot(X_pca.iloc[i, 0], X_pca.iloc[i, 1], 'o', markerfacecolor=col,
-             markeredgecolor='k', markersize=6)
-plt.scatter(X_pca.iloc[:,0], X_pca.iloc[:,1])
-plt.show()
+plot_3d(covid_df['Confirmed'], covid_df['Deaths'], covid_df['Recovered'], labels=out_label, cmap='brg')
+#    plt.plot(X_pca.iloc[i, 0], X_pca.iloc[i, 1], 'o', markerfacecolor=col,
+#             markeredgecolor='k', markersize=6)
+#plt.scatter(X_pca.iloc[:,0], X_pca.iloc[:,1])
+#plt.show()
 
 outlier = []
 print("Los outliers son:")
@@ -129,30 +139,30 @@ for i in outliers:
     outlier.append(covid_df.iloc[i,:])
     covid_df = covid_df.drop(i) #Remove the outlier from data
     
-threedee = plt.figure().gca(projection='3d')
-threedee.scatter(covid_df['Confirmed'], covid_df['Deaths'], covid_df['Recovered'])
-threedee.set_xlabel('Confirmed')
-threedee.set_ylabel('Deaths')
-threedee.set_zlabel('Recovered')
-plt.show()
+# print data without outlier
+plot_3d(covid_df['Confirmed'], covid_df['Deaths'], covid_df['Recovered'])
 
-gm = GaussianMixture(n_components=K, max_iter=100, tol=0.00001)
+# 6. Perform Clustering
+gm = GaussianMixture(n_components=K, max_iter=100, tol=0.000001)
 
 gm.fit(covid_df.drop(['Country/Region'], axis=1))
 labels = gm.predict(covid_df.drop(['Country/Region'], axis=1))
 
-#plt.scatter(X[:, 0], X[:, 1], c=labels, s=40, cmap='viridis');
 
-threedee = plt.figure().gca(projection='3d')
-threedee.scatter(covid_df['Confirmed'], covid_df['Deaths'], covid_df['Recovered'], c=labels, cmap='viridis')
-threedee.set_xlabel('Confirmed')
-threedee.set_ylabel('Deaths')
-threedee.set_zlabel('Recovered')
-plt.show()
-    
+plot_3d(covid_df['Confirmed'], covid_df['Deaths'], covid_df['Recovered'], labels=labels, cmap='brg')
 
-    
+# 7. Analysis Data
 
+covid_df['labels'] = labels
+
+groups = {}
+
+for i in range(K):
+    groups[i] = covid_df.loc[covid_df['labels'] == i]
+
+for i in range(K):
+    print(f"Group {i}:")
+    print(groups[i].mean())
 
 
 
